d2cf4d8c00d1812707dd9cb072c47b0610553b7d
Pawel Juraszek <pawel.jkuraszek1994@gmail.com>
2020-01-17 18:58:46 +0100

    Cleanup and use static methods where IDE suggested.

--- file_uploader.py
+++ file_uploader.py
@@ -26,7 +26,7 @@ class FileUploader:
     query_list = []
 
     def __init__(self):
-        """ Initialize class, update current date and prepare default folder name. """
+        """ Initialize class instance, update current date and prepare default folder name. """
         self.execute_date = date.today().strftime("%B %d, %Y")
         self.folder_name = ('Files uploaded ' + date.today().strftime("%B %d, %Y"))
         self.query_list = []
@@ -77,7 +77,8 @@ class FileUploader:
         # Save stuff on drive and hard disk:
         self.save_attachments(self.mail_service, self.py_drive, 'me', attachment_data, folder_id, save=True)
 
-    def create_message(self, sender, to, subject, message_text):
+    @staticmethod
+    def create_message(sender, to, subject, message_text):
         """ Create a message for an email.
         Args:
             sender: Email address of the sender.
@@ -93,7 +94,8 @@ class FileUploader:
         message = base64.urlsafe_b64encode(message.as_bytes())
         return {'raw': message.decode('utf-8')}
 
-    def send_message(self, mail_service, user_id, message):
+    @staticmethod
+    def send_message(mail_service, user_id, message):
         """ Send an email message.
         Args:
             mail_service: Authorized Gmail API instance.
@@ -108,7 +110,17 @@ class FileUploader:
         except errors.HttpError as error:
             print('An error occurred: %s' % {error})
 
-    def ids_of_messages_matching_query(self, mail_service, user_id, query_list):
+    # Send email example:
+
+    # email_sender = 'example@gmail.com'
+    # email_receivers = 'example@gmail.com'
+    # email_subject = 'Test'
+    # email_content = 'Hello, this is a test'
+    # body = create_message(email_sender, email_receivers, email_subject, email_content)
+    # mail_service.users().messages().send(userId='me', body=body).execute()
+
+    @staticmethod
+    def ids_of_messages_matching_query(mail_service, user_id, query_list):
         """ List all Messages of the user's mailbox matching the query.
         Args:
             mail_service: Authorized Gmail API instance.
@@ -139,7 +151,8 @@ class FileUploader:
             emails_ids.append(i['id'])
         return emails_ids
 
-    def get_attachments_ids(self, mail_service, user_id, emails_ids):
+    @staticmethod
+    def get_attachments_ids(mail_service, user_id, emails_ids):
         """ Get all attachments IDs from provided emails IDs.
         Args:
           mail_service: Authorized Gmail API instance.
@@ -183,7 +196,8 @@ class FileUploader:
         return {'Emails IDs': emails_id, 'Attachments IDs': attachment_ids,
                 'Attachments file names': attachments_file_names, 'mail data': mail_data}
 
-    def save_attachments(self, mail_service, py_drive, user_id, attachment_data, drive_folder_id, save=False):
+    @staticmethod
+    def save_attachments(mail_service, py_drive, user_id, attachment_data, drive_folder_id, save=False):
         """ Get and save attachments on user GDrive, with option to save them on hard drive.
         Args:
             mail_service: Authorized Gmail API instance.
@@ -254,7 +268,8 @@ class FileUploader:
         except errors.HttpError as error:
             print('An error occurred: %s' % {error})
 
-    def create_new_folder(self, drive_service, folder_name, parent_folder_id):
+    @staticmethod
+    def create_new_folder(drive_service, folder_name, parent_folder_id):
         """ Create folder on Google Drive
         Args:
             drive_service: Authorized Gmail API drive_service instance.
@@ -279,37 +294,7 @@ class FileUploader:
 
 
 def main():
-    # google_auth = GoogleAuth()
-    # google_auth.LocalWebserverAuth()
-    # google_auth.Authorize()
-    #
-    # mail_service = build('gmail', 'v1', credentials=google_auth.credentials)  # Gmail API
-    # drive_service = build('drive', 'v3', credentials=google_auth.credentials)  # Drive API
-    # py_drive = GoogleDrive(google_auth)  # PyDrive Drive API
-
-    # Examples:
-    # Send email example:
-
-    # email_sender = 'example@gmail.com'
-    # email_receivers = 'example@gmail.com'
-    # email_subject = 'Test'
-    # email_content = 'Hello, this is a test'
-    # body = create_message(email_sender, email_receivers, email_subject, email_content)
-    # mail_service.users().messages().send(userId='me', body=body).execute()
-
-    # File_uploader example:
-    # Search for emails that contain any items in query list, then save them to desired folder in google drive.
-    # Check for any emails matching query:
-    # query = ['label:Faktury']
-    # emails_ids = ids_of_messages_matching_query(mail_service, 'me', query)
-    # # Search for folder ID:
-    # folder_name = 'Folder na faktury'
-    # folder_id = (search_for_file_id(drive_service, "mimeType='application/vnd.google-apps.folder'", folder_name))
-    # # Data from emails:
-    # attachment_data = get_attachments_ids(mail_service, 'me', emails_ids)
-    # # Save stuff on drive and hard disk:
-    # save_attachments(mail_service, py_drive, 'me', attachment_data, folder_id, save=True)
-
+    # User inputs:
     queries = ['label:Faktury']
     folder_name = 'Folder na faktury'
     FileUploader().execute(queries, folder_name)
