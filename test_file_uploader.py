67024b807ac458eb424581d49e9b4d84364c5bae
Pawel Juraszek <pawel.jkuraszek1994@gmail.com>
2020-01-22 13:37:03 +0100

    test: Added file for unit testing.

---
+++ test_file_uploader.py
@@ -0,0 +1,27 @@
+import unittest
+
+
+class TestFileUploader(unittest.TestCase):
+    def test_create_new_folder(self):
+        pass
+
+    def test_search_for_file_id(self):
+        pass
+
+    def test_save_attachments(self):
+        pass
+
+    def test_get_attachments_ids(self):
+        pass
+
+    def test_ids_of_messages_matching_query(self):
+        pass
+
+    def test_upload_files(self):
+        pass
+
+    def test_refresh_services(self):
+        pass
+
+    def test_authentication(self):
+        pass
